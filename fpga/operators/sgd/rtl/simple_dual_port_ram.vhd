a080341ff2aef24c52880d2ac1bf9ac89f12a659
David Sidler <dasidler@inf.ethz.ch>
2017-05-02 21:49:32 +0200

    Added GPLv3 license to hardware operators

--- fpga/operators/sgd/rtl/simple_dual_port_ram.vhd
+++ fpga/operators/sgd/rtl/simple_dual_port_ram.vhd
@@ -1,3 +1,19 @@
+--
+--  Copyright 2016 - 2017 Systems Group, ETH Zurich
+-- 
+--  This hardware operator is free software: you can redistribute it and/or
+--  modify it under the terms of the GNU General Public License as published
+--  by the Free Software Foundation, either version 3 of the License, or
+--  (at your option) any later version.
+-- 
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+-- 
+--  You should have received a copy of the GNU General Public License
+--  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
 -- Quartus II VHDL Template
 -- Simple Dual-Port RAM with different read/write addresses but
 -- single read/write clock
